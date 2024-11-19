071ed80b753d6af1cbb64c292e36e096f3b9bf86
maxwellharris0303 <oscarrogers303@gmail.com>
2024-11-19 11:20:38 -0800

    enhanced stock checker

--- stock_checker.py
+++ stock_checker.py
@@ -5,7 +5,7 @@ import undetected_chromedriver as uc
 # Initialize the undetected Chrome driver
 driver = uc.Chrome(version_main=130)
 driver.maximize_window()
-driver.get("https://yesgk.com/products/dragon-ball-standing-frieza-second-form-resin-statue-break-studio-pre-order")
+driver.get("https://www.warhammer.com/en-WW/shop/tau-empire-retaliation-cadre-2024?queryID=cbc34c8e69f72de08dedcc3ada1205c2&srsltid=AfmBOooDat22iM61Zb2rlyP_c0tVuaH5999wo6OOSGSMT2w2Z0Au2qqz")
 
 def get_result(content):
     soup = BeautifulSoup(content, 'html.parser')
@@ -15,14 +15,24 @@ def get_result(content):
         tag.decompose()
 
     # Define the regex pattern for full match
-    pattern = re.compile(r"^(out of stock|sold out|unavailable)$", re.IGNORECASE)
+    pattern_out_stock = re.compile(r"^(out of stock|sold out|unavailable)$", re.IGNORECASE)
+    pattern_in_stock = re.compile(r"^\s*\d*\s*(in stock|add to cart|pre-order|pre-order now|add to wishlist|available)$", re.IGNORECASE)
 
+    out_stock_array = []
+    in_stock_array = []
     # Iterate through all elements
     for element in soup.find_all(True):
         # Get the stripped text content of the element
         text = element.get_text(strip=True)
-        if text and re.match(pattern, text):
-            print(f"Exact match found: {text}")
+        if text and re.match(pattern_out_stock, text):
+            out_stock_array.append(text.lower())
+        if text and re.match(pattern_in_stock, text):
+            in_stock_array.append(text.lower())
+    
+    out_stock_array = list(set(out_stock_array))
+    in_stock_array = list(set(in_stock_array))
+    print(f"Out of stock match found: {out_stock_array}")
+    print(f"In stock match found: {in_stock_array}")
 
 # Pass the page source to the function
 get_result(driver.page_source)
